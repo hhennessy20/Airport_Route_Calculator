56a7b25db47012c2b916f5d2b22d1edcaac9f468
hhennessy20 <hhennessy20@gmail.com>
2023-10-03 15:39:37 -0400

    Drop Stops column
    Stops column is removed after flights with more than 0 stops are thrown out.

--- initialize_data.py
+++ initialize_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 airports = pd.read_csv("airports.csv")
 airports = airports.drop(['ICAO', 'Altitude', 'Timezone', 'DST', 'Timezone.1'], axis=1)
 routes = pd.read_csv("routes.csv")
-routes = routes.drop(['Source Airport ID', 'Destination Airport ID', 'Codeshare', 'Equipment'], axis=1)
 routes = routes.drop(routes[routes.Stops > 0].index)
+routes = routes.drop(['Source Airport ID', 'Destination Airport ID', 'Codeshare', 'Equipment', 'Stops'], axis=1)
 print(airports.sample(10))
 print(routes.sample(10))
